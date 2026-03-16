cca2c3dd70c30dc35dc488fc7dcb85bbbeed515d
Lucas Brandalise <codoedits@gmail.com>
2026-03-16 16:08:40 -0300

    Use env var for SECRET_KEY and load dotenv

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$nv+*(nom&3n#w+eeom!e0^ece5f8$2x-n!hradu93!dp*d20('
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-please-change-this')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
