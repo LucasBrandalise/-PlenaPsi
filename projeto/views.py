bb53d22a366fb81793910fc5dfe6f2fac5ee6070
Lucas Brandalise <codoedits@gmail.com>
2026-03-17 01:35:29 -0300

    Atualiza rotas, views, templates e navbar e atualiza requirements

--- projeto/views.py
+++ projeto/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'index.html')
+
+def clinica(request):
+    return render(request, 'clinica.html')
+
+def pericia(request):
+    return render(request, 'pericia.html')
+
+def cursos(request):
+    return render(request, 'cursos.html')
+
+def sobre(request):
+    return render(request, 'sobre.html')
+
+def contato(request):
+    return render(request, 'contato.html')
