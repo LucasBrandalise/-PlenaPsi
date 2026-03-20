2b0bb523d0886655109b63421668e3c64204bf22
Lucas Brandalise <codoedits@gmail.com>
2026-03-20 16:36:10 -0300

    atualizacao de rotas

--- projeto/urls.py
+++ projeto/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('clinicas/', views.clinica, name='clinica'),
+    path('pericia/', views.pericia, name='pericia'),
+    path('cursos/', views.cursos, name='cursos'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('contato/', views.contato, name='contato'),
+
 ]
