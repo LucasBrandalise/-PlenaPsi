f7c8c04fe765e90f520860d9fdcc725dbf8c87ea
Lucas Brandalise <codoedits@gmail.com>
2026-04-11 17:00:05 -0300

    atualizando rotas

--- projeto/views.py
+++ projeto/views.py
@@ -1,23 +1,7 @@
 from django.shortcuts import render
 
-
-def index(request):
-    return render(request, 'index.html')
-
 def home(request):
     return render(request, 'index.html')
 
-def clinica(request):
-    return render(request, 'clinica.html')
-
-def pericia(request):
-    return render(request, 'pericia.html')
-
-def cursos(request):
-    return render(request, 'cursos.html')
-
-def sobre(request):
-    return render(request, 'sobre.html')
-
-def contato(request):
-    return render(request, 'contato.html')
+def index(request):
+    return render(request, 'index.html')
--- setup/urls.py
+++ setup/urls.py
@@ -3,12 +3,6 @@ from django.urls import path
 from projeto import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('index.html', views.index, name='index'),
-    path('clinica/', views.clinica, name='clinica'),
-    path('pericia/', views.pericia, name='pericia'),
-    path('cursos/', views.cursos, name='cursos'),
-    path('sobre/', views.sobre, name='sobre'),
-    path('contato/', views.contato, name='contato'),
 ]
